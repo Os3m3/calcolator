eafc2e17e01c967822f0920474740ddfb7cff2c1
Osama Said Al Zakwani <Osama.Zakwani@gmail.com>
2025-05-14 15:01:09 +0400

    The + operator

---
+++ calc.py
@@ -0,0 +1,9 @@
+firstNumber = float(input("Enter the First Number: "))
+operator = input("Enter the Operator: ")
+secondNumber = float(input("Enter the Second Number: "))
+
+if operator == "+":
+    print("The Answer is:", firstNumber + secondNumber)
+
+else:
+    print("Please Try Again Sad :(")

efbddc28d440583b71d2caadc48291d38eb89279
Osama Said Al Zakwani <Osama.Zakwani@gmail.com>
2025-05-14 15:05:35 +0400

    The Abs operator

--- calc.py
+++ calc.py
@@ -2,10 +2,8 @@ firstNumber = float(input("Enter the First Number: "))
 operator = input("Enter the Operator: ")
 secondNumber = float(input("Enter the Second Number: "))
 
-
-if operator == "^":
-    print("The Answer is:", pow(firstNumber, secondNumber))
-
+if operator == "abs":
+    print("The Answer is:", abs(firstNumber + secondNumber))
 
 else:
     print("Please Try Again Sad :(")

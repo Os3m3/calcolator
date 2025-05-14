0dcef7c9a6baa9aced25395ae9c3d23baea3c4da
Osama Said Al Zakwani <Osama.Zakwani@gmail.com>
2025-05-14 15:09:38 +0400

    The All operator

--- calc.py
+++ calc.py
@@ -3,9 +3,26 @@ operator = input("Enter the Operator: ")
 secondNumber = float(input("Enter the Second Number: "))
 
 
-if operator == "*":
+if operator == "+":
+    print("The Answer is:", firstNumber + secondNumber)
+
+elif operator == "-":
+    print("The Answer is:", firstNumber - secondNumber)
+
+elif operator == "*":
     print("The Answer is:", firstNumber * secondNumber)
 
+elif operator == "/":
+    print("The Answer is:", firstNumber / secondNumber)
+
+elif operator == "^":
+    print("The Answer is:", pow(firstNumber, secondNumber))
+
+elif operator == "abs":
+    print("The Answer is:", abs(firstNumber + secondNumber))
+
+elif operator == "%":
+    print("The Answer is:", firstNumber % secondNumber)
 
 else:
     print("Please Try Again Sad :(")

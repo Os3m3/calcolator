cdf0f1f106eacb11f79e54ea905eefe83ae92b15
Osama Said Al Zakwani <Osama.Zakwani@gmail.com>
2025-05-14 15:06:09 +0400

    The % operator

--- calc.py
+++ calc.py
@@ -2,8 +2,9 @@ firstNumber = float(input("Enter the First Number: "))
 operator = input("Enter the Operator: ")
 secondNumber = float(input("Enter the Second Number: "))
 
-if operator == "abs":
-    print("The Answer is:", abs(firstNumber + secondNumber))
+
+if operator == "%":
+    print("The Answer is:", firstNumber % secondNumber)
 
 else:
     print("Please Try Again Sad :(")
